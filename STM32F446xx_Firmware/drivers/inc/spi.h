5362cf9aa484e036860d1bd33913d110b2a2d2a6
MohamoudAli12 <63305673+MohamoudAli12@users.noreply.github.com>
2024-06-21 22:03:07 +1000

    spi.c added new api's

--- STM32F446xx_Firmware/drivers/inc/spi.h
+++ STM32F446xx_Firmware/drivers/inc/spi.h
@@ -5,31 +5,59 @@
 
 typedef enum
 {
-    x,
-    y
+    SPI_CLK_SPEED_8MHZ,
+    SPI_CLK_SPEED_4MHZ,
+    SPI_CLK_SPEED_2MHZ,
+    SPI_CLK_SPEED_1MHZ,
+    SPI_CLK_SPEED_500KHZ,
+    SPI_CLK_SPEED_250KHZ,
+    SPI_CLK_SPEED_125KHZ,
+    SPI_CLK_SPEED_62K5HZ
+}spi_clk_speed_t;
+
+
+typedef enum
+{
+    SPI_MODE_SLAVE,
+    SPI_MODE_MASTER
 } spi_config_master_slave_t;
 
 typedef enum
 {
-    x,
-    y,
-    z
+    SPI_FULL_DUPLEX,
+    SPI_HALF_DUPLEX,
+    SPI_SIMPLEX_TXONLY,
+    SPI_SIMPLEX_RXONLY
 }spi_config_comms_mode_t;
 
 
 typedef enum
 {
-    x,
-    y
+    SPI_8_BIT_DATA,
+    SPI_16_BIT_DATA
 }spi_config_data_length_t;
 
 
 typedef enum
 {
-    x,
-    y
+    SPI_CPHA_LOW,
+    SPI_CPHA_HIGH
 
-}spi_config_cpha_cpol_t;
+}spi_config_cpha_t;
+
+typedef enum
+{
+    SPI_CPOL_LOW,
+    SPI_CPOL_HIGH
+
+}spi_config_cpol_t;
+
+typedef enum
+{
+    SPI_HW_SLAVE_MANAGE,
+    SPI_SW_SLAVE_MANAGE
+
+}spi_config_slave_manage_t;
 
 typedef enum 
 {
@@ -37,20 +65,20 @@ typedef enum
     SPI_PIN_SET      
 } spi_pin_state_t;
 
-
+void config_spi_speed(spi_register_def_t *p_spi_x,spi_clk_speed_t clk_speed);
 void spi_peripheral_clk_cntrl(spi_register_def_t *p_spi_x, spi_pin_state_t enable_or_disable);
+void config_spi_baud_rate();
 
-void config_spi_as_slave(spi_register_def_t *p_spi_x);
 
-void config_spi_as_master(spi_register_def_t *p_spi_x, spi_config_master_slave_t master_slave);
+void config_spi_as_master_or_slave(spi_register_def_t *p_spi_x, spi_config_master_slave_t master_or_slave);
 
 void config_spi_comm_mode(spi_register_def_t *p_spi_x, spi_config_comms_mode_t comm_mode);
 
 void config_spi_data_length(spi_register_def_t *p_spi_x, spi_config_data_length_t data_length);
 
-void config_spi_cpha_cpol(spi_register_def_t *p_spi_x,spi_config_cpha_cpol_t cpha_cpol);
+void config_spi_cpha(spi_register_def_t *p_spi_x,spi_config_cpha_t cpha);
+void config_spi_cpol(spi_register_def_t *p_spi_x,spi_config_cpol_t cpol);
 
-void config_spi_speed(spi_register_def_t *p_spi_x);
 
 
 
--- STM32F446xx_Firmware/drivers/src/spi.c
+++ STM32F446xx_Firmware/drivers/src/spi.c
@@ -44,3 +44,36 @@ void spi_peripheral_clk_cntrl(spi_register_def_t *p_spi_x, spi_pin_state_t enabl
     }
 
 }
+
+void config_spi_speed(spi_register_def_t *p_spi_x, spi_clk_speed_t clk_speed)
+{
+    const uint8_t spi_cr1_br_mask =0x07;
+    p_spi_x->SPI_CR1 &= ~(spi_cr1_br_mask<<BIT_3);
+    p_spi_x->SPI_CR1 |= (clk_speed << BIT_3);
+
+}
+
+void config_spi_as_master_or_slave(spi_register_def_t *p_spi_x, spi_config_master_slave_t master_or_slave)
+{
+    p_spi_x->SPI_CR1 &= ~(0x01 << BIT_2);
+    p_spi_x->SPI_CR1 |= (master_or_slave << BIT_2);    
+}
+
+
+void config_spi_data_length(spi_register_def_t *p_spi_x, spi_config_data_length_t data_length)
+{
+    p_spi_x->SPI_CR1 &= ~(0x01 << BIT_11);
+    p_spi_x->SPI_CR1 |= (data_length << BIT_11);
+}
+
+void config_spi_cpha(spi_register_def_t *p_spi_x,spi_config_cpha_t cpha)
+{
+    p_spi_x->SPI_CR1 &= ~(0x01 << BIT_0);
+    p_spi_x->SPI_CR1 |= (cpha << BIT_0);
+}
+
+void config_spi_cpol(spi_register_def_t *p_spi_x,spi_config_cpol_t cpol)
+{
+    p_spi_x->SPI_CR1 &= ~(0x01 << BIT_1);
+    p_spi_x->SPI_CR1 |= (cpol << BIT_1);
+}
