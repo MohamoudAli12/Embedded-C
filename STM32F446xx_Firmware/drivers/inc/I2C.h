59ae52bf14b216a69c7dcec26bfc13f1139deb19
MohamoudAli12 <63305673+MohamoudAli12@users.noreply.github.com>
2024-07-25 22:10:25 +1000

    i2c driver dev

--- STM32F446xx_Firmware/drivers/inc/I2C.h
+++ STM32F446xx_Firmware/drivers/inc/I2C.h
@@ -11,15 +11,41 @@
 
 #include "stm32f446xx.h"
 
+#define I2C_CR1_PE                                         0
+#define I2C_CCR_FS                                         15
 
 
+typedef enum 
+{
+    I2C_SPEED_STANDARD,
+    I2C_SPEED_FAST
+}i2c_speed_t;
+
+typedef enum
+{
+    I2C_SLAVE_TX,
+    I2C_SLAVE_RX,
+    I2C_MASTER_TX,
+    I2C_MASTER_RX
+}i2c_mode_t;
 
 
+typedef enum
+{
+    I2C_FREQ_2MHZ = 0x02
 
 
+}i2c_freq_t;
 
+void i2c_peripheral_enable(i2c_register_def_t *p_i2c_x);
 
+void i2c_peripheral_disable(i2c_register_def_t *p_i2c_x);
 
+void i2c_sm_mode_config(i2c_register_def_t *p_i2c_x);
+
+void i2c_fm_mode_config(i2c_register_def_t *p_i2c_x);
+
+void i2c_own_addr_config(i2c_register_def_t *p_i2c_x, uint8_t own_addr);
 
 
 
---
+++ STM32F446xx_Firmware/drivers/src/i2c.c
@@ -0,0 +1,21 @@
+#include "i2c.h"
+
+i2c_peripheral_enable(i2c_register_def_t *p_i2c_x)
+{
+    p_i2c_x->I2C_CR1 |= (1<<I2C_CR1_PE);
+}
+
+void i2c_peripheral_disable(i2c_register_def_t *p_i2c_x)
+{
+    p_i2c_x->I2C_CR1 &= ~(1<<I2C_CR1_PE);
+}
+
+void i2c_sm_mode_config(i2c_register_def_t *p_i2c_x)
+{
+    p_i2c_x->I2C_CR2 &= ~(1<<I2C_CCR_FS);
+}
+
+void i2c_fm_mode_config(i2c_register_def_t *p_i2c_x)
+{
+    p_i2c_x->I2C_CR2 |= (1<<I2C_CCR_FS);
+}
