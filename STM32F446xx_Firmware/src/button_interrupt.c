2fb781dd5a105ec801cc91ae571a9416e636f593
MohamoudAli12 <63305673+MohamoudAli12@users.noreply.github.com>
2025-06-02 21:35:02 +1000

    updated gitignore

--- STM32F446xx_Firmware/src/button_interrupt.c
+++ STM32F446xx_Firmware/src/button_interrupt.c
@@ -1,45 +1,38 @@
-#include <stdint.h>
-#include "stm32f446xx.h"
-#include "gpio.h"
 #include "fn_prototypes.h"
+#include "gpio.h"
+#include "stm32f446xx.h"
+#include <stdint.h>
 
-#define BUTTON_PRESSED                      0
 
-
-
-
+#define BUTTON_PRESSED 0
 
 void button_interrupt_led_toggle(void)
 {
-    //configure Led pin
-    
-    GPIOA_PCLK_EN();
-    gpio_output_mode_config(GPIOA, GPIO_PIN_5, GPIO_SPEED_FAST, GPIO_OTYPE_PUSH_PULL);
-    gpio_pull_type_config(GPIOA, GPIO_PIN_5, GPIO_NO_PULL_UP_DOWN);
+  // configure Led pin
 
-    //configure button
+  GPIOA_PCLK_EN();
+  gpio_output_mode_config(GPIOA, GPIO_PIN_5, GPIO_SPEED_FAST, GPIO_OTYPE_PUSH_PULL);
+  gpio_pull_type_config(GPIOA, GPIO_PIN_5, GPIO_NO_PULL_UP_DOWN);
 
-    GPIOC_PCLK_EN();
-    gpio_intrpt_config(GPIOC_INTRPT_SRC, GPIO_PIN_13, GPIO_INTRPT_EDGE_FALL);
-    irq_enable_or_disable(EXTI15_10_IRQ_POS, ENABLE);
-  
-  
-    while(1)
+  // configure button
+
+  GPIOC_PCLK_EN();
+  gpio_intrpt_config(GPIOC_INTRPT_SRC, GPIO_PIN_13, GPIO_INTRPT_EDGE_FALL);
+  irq_enable_or_disable(EXTI15_10_IRQ_POS, ENABLE);
+
+  while (1)
+  {
+    if (gpio_read_from_input_pin(GPIOC, GPIO_PIN_13) == BUTTON_PRESSED)
     {
-    if(gpio_read_from_input_pin(GPIOC, GPIO_PIN_13)==BUTTON_PRESSED ) 
-    {
-        
-        EXTI15_10_IRQHandler();
-    }
-    }
 
+      EXTI15_10_IRQHandler();
+    }
+  }
 }
 
 void EXTI15_10_IRQHandler(void)
 {
-    
-    gpio_clear_irq_pending(GPIO_PIN_13);
-    gpio_toggle_output_pin(GPIOA,GPIO_PIN_5);
-   
 
+  gpio_clear_irq_pending(GPIO_PIN_13);
+  gpio_toggle_output_pin(GPIOA, GPIO_PIN_5);
 }
